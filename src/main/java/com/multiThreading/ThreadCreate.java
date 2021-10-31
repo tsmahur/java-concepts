4176fa69497ed7688bc1b08cfb6dec5249f8c238
tsmahur <32067811+tsmahur@users.noreply.github.com>
2021-11-01 01:59:07 +0530

    Multithreading - anonymous thread, runnable

--- src/main/java/com/multiThreading/ThreadCreate.java
+++ src/main/java/com/multiThreading/ThreadCreate.java
@@ -4,11 +4,23 @@ public class ThreadCreate extends Thread {
     public static void main(String[] args) throws InterruptedException {
 
         //Two ways to create thread
-        //1. implementing Runnable interface
+        //1.a. implementing Runnable interface
         MyRunnable myRunnable =new MyRunnable();
         Thread threadUsingRunnable= new Thread(myRunnable);
         threadUsingRunnable.start();
         System.out.println("thread using Runnable interface is started name: "+threadUsingRunnable.getName());
+
+        //1.b. implementing Runnable interface - AnonymousRunnable
+        Runnable anonymousRunnable = () -> System.out.println("thread using AnonymousRunnable is running.... name : "+Thread.currentThread().getName());
+//        Runnable anonymousRunnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("thread using AnonymousRunnable is running.... name : "+Thread.currentThread().getName());
+//            }
+//        };
+        Thread threadUsingAnonymousRunnable= new Thread(anonymousRunnable);
+        threadUsingAnonymousRunnable.start();
+        System.out.println("thread using threadUsingAnonymousRunnable is started name: "+threadUsingAnonymousRunnable.getName());
 
         //2.a. extending Thread class
         MyThread thread=new MyThread();
@@ -26,6 +38,17 @@ public class ThreadCreate extends Thread {
         Thread threadUsingRunnableAndNameConstructor= new Thread(myRunnable2,"threadUsingRunnableAndNameConstructor");
         threadUsingRunnableAndNameConstructor.start();
         System.out.println("thread using Thread(Runnable r,String Name) is started name: "+threadUsingRunnableAndNameConstructor.getName());
+
+        //2.d. Thread Class - Anonymous
+        Thread threadUsingAnonymousThread= new Thread(() -> System.out.println("thread using AnonymousThread is running.... name : "+Thread.currentThread().getName()));
+//        Thread threadUsingAnonymousThread= new Thread(){
+//            @Override
+//            public void run() {
+//                System.out.println("thread using MyRunnable is running.... name : "+Thread.currentThread().getName());
+//            }
+//        };
+        threadUsingAnonymousThread.start();
+        System.out.println("thread using Thread(()->{}) AnonymousThread is started name: "+threadUsingAnonymousThread.getName());
 
     }
 
