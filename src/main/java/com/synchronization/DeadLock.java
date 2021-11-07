a47bc768a674cbcfc273fbf201c1579dddb3fd6f
tsmahur <32067811+tsmahur@users.noreply.github.com>
2021-11-07 16:22:04 +0530

    synchronized - updated deadlock code

--- src/main/java/com/synchronization/DeadLock.java
+++ src/main/java/com/synchronization/DeadLock.java
@@ -7,44 +7,26 @@ public class DeadLock {
 
         //making deadlock
         Thread threadUsingAnonymousThread1= new Thread(() -> {
-//            synchronized (resource1) {
-//                System.out.println(Thread.currentThread().getName() + ", resource1 : " + resource1);
-//
-////                System.out.println(Thread.currentThread().getName() + ", waiting for resource2 ");
-//                try { Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
-//                synchronized (resource2) {
-//                    System.out.println(Thread.currentThread().getName() + ", resource2 : " + resource2);
-//                }
-//            }
             synchronized (resource1) {
-                System.out.println(Thread.currentThread().getName() +": locked resource 1 : "+resource1);
+                System.out.println(Thread.currentThread().getName() + ",locked-> resource1 : " + resource1);
 
-                try { Thread.sleep(100);} catch (Exception e) {e.printStackTrace();}
-
+                System.out.println(Thread.currentThread().getName() + ", waiting for resource2 ");
+                try { Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
                 synchronized (resource2) {
-                    System.out.println(Thread.currentThread().getName() +": locked resource 2 : "+resource2);
+                    System.out.println(Thread.currentThread().getName() + ",locked-> resource2 : " + resource2);
                 }
             }
 
         },"thread1");
         Thread threadUsingAnonymousThread2= new Thread(() -> {
-//            synchronized (resource1) {
-//                System.out.println(Thread.currentThread().getName() + ", resource2 : " + resource2);
-//
-////                System.out.println(Thread.currentThread().getName() + ", waiting for resource1 ");
-//                try { Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
-//
-//                synchronized (resource2) {
-//                    System.out.println(Thread.currentThread().getName() + ", resource1 : " + resource1);
-//                }
-//            }
             synchronized (resource2) {
-                System.out.println(Thread.currentThread().getName() +": locked resource 2 : "+resource2);
+                System.out.println(Thread.currentThread().getName() + ",locked-> resource2 : " + resource2);
 
-                try { Thread.sleep(100);} catch (Exception e) {e.printStackTrace();}
+                System.out.println(Thread.currentThread().getName() + ", waiting for resource1 ");
+                try { Thread.sleep(100);} catch (InterruptedException e) {e.printStackTrace();}
 
                 synchronized (resource1) {
-                    System.out.println(Thread.currentThread().getName() +" locked resource 1 : "+resource1);
+                    System.out.println(Thread.currentThread().getName() + ",locked-> resource1 : " + resource1);
                 }
             }
 
